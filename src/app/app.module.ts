ec4fbfd4eb3c0a0524fe170fbbaccc97532df775
suyasharya31 <aryasuyash39@gmail.com>
2020-06-19 20:59:39 +0530

    click OnSelect to dispaly details and comments of a dish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,30 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import {MatListModule} from '@angular/material/list';
 import { MenuComponent } from './menu/menu.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 import 'hammerjs';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatGridListModule,
+    MatToolbarModule,
     MatListModule,
-    MatToolbarModule
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,3 +1,4 @@
+import {Comment} from './Comment';
 export class Dish
 {
   id:string;
@@ -8,4 +9,5 @@ export class Dish
   label:string;
   price:string;
   description:string;   
+  comments:Comment[];
 }
