0cea486a140b992b1263c0a40c1ef694d51bc045
suyasharya31 <aryasuyash39@gmail.com>
2020-06-19 12:08:27 +0530

    first commit ( mattoolbar)

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MenuComponent } from './menu/menu.component';
+import 'hammerjs';
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    
+    MatToolbarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
