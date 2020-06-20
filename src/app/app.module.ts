d666a54a11a01105340bab65b042eafcdf0efc0f
suyasharya31 <aryasuyash39@gmail.com>
2020-06-20 13:49:04 +0530

    header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,11 +13,15 @@ import {MatButtonModule} from '@angular/material/button';
 import 'hammerjs';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
